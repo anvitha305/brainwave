8d95dfdbeb773e8d5863a2facf0ed9fa65fdf039
Anvitha <anvitha2k20@gmail.com>
2023-05-30 18:09:20 -0400

    changing the design of brainwav [primarily just switching to c++]

---
+++ brainwav.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include "brainwav.h"
+using namespace std;
+
+
+// returns note on C diminished scale 
+string bf_note(char symbol){
+    switch(symbol){
+	    case '>':
+		    return "C";
+		    break;
+	case "<":
+
+    }
+}
+
+int calc_freq(char* note){
+}
+
+
--- brainwav.h
+++ brainwav.h
@@ -6,10 +6,8 @@
 #ifndef MAKE_WAV_H
 #define MAKE_WAV_H
 
-// port the brainfuck symbol to a note on the octatonic scale.
+// port the brainfuck symbol to a note on the octatonic scale. [here refers to C diminished]
 char* bf_note(char symbol);
 // calculate frequency of a note such that it can be written to the wav file
 int calc_freq(char *note);
-// writing the data to a wav file
-void write_to_wav(char *fname, unsigned long samp, short int *data, int s_rate);
 #endif
